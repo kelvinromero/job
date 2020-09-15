0ee1cb1b629469194c8f96560ba62f40705c2981
Kelvin Romero <kelvinromero@gmail.com>
2020-09-14 21:01:39 -0300

    Move user syncing to model. Make worker idempotent (also lean).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,40 @@
 class User < ApplicationRecord
+
+  def sync_on_remote!
+    return unless (users = get_users)
+    return if user_exists_on_remote(users, self)
+
+    if (remote_user = post_user(self))
+      self.remote_id = remote_user["id"]
+      self.save!
+    end
+  end
+
+  private
+  # TODO: Move API calls to services (Or interactors maybe
+  # )
+  def get_users
+    begin
+      uri = URI('https://reqres.in/api/users?per_page=12')
+      res = Net::HTTP.get_response(uri)
+      JSON.parse(res.body)['data']
+    rescue SocketError
+      return false
+    end
+  end
+
+  def post_user(local_user)
+    begin
+      uri = URI('https://reqres.in/api/users')
+      user = { first_name: local_user.first_name, last_name: local_user.last_name }
+      res = Net::HTTP.post_form(uri, user)
+      JSON.parse(res.body)
+    rescue SocketError
+      return false
+    end
+  end
+
+  def user_exists_on_remote(users, local_user)
+    users.select { |user| user["first_name"] == local_user.first_name and user["last_name"] == local_user.last_name }.any?
+  end
 end
--- app/workers/sync_user_worker.rb
+++ app/workers/sync_user_worker.rb
@@ -2,36 +2,10 @@ require 'net/http'
 
 class SyncUserWorker
   include Sidekiq::Worker
-  sidekiq_options :queue => :default , :retry => 1
 
   def perform(user_id)
-    local_user = User.find(user_id)
-
-    unless user_exists(get_users, local_user)
-      if (remote_user = post_user(local_user))
-        remote_user
-      end
-    end
+    user = User.find(user_id)
+    return unless user.remote_id.nil?
+    user.sync_on_remote!
   end
-
-  private
-  def get_users
-    uri = URI('https://reqres.in/api/users?per_page=12')
-    res = Net::HTTP.get_response(uri)
-    JSON.parse(res.body)['data']
-  end
-
-  def post_user(local_user)
-    uri = URI('https://reqres.in/api/users?per_page=12')
-    user = { first_name: local_user.first_name, last_name: local_user.last_name }
-    res = Net::HTTP.post_form(uri, user)
-    JSON.parse(res.body)
-  end
-
-  def user_exists(users, local_user)
-    users.select do |user|
-      user["first_name"] == local_user.first_name and user["last_name"] == local_user.last_name
-    end
-  end
-
 end
