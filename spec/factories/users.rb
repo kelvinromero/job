9c65c666e85d751843aa44c2e9baf2091a69e71c
Kelvin Romero <kelvinromero@gmail.com>
2020-09-16 00:32:08 -0300

    Adds invalid and synced traits

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,14 @@ FactoryBot.define do
   factory :user do
     first_name { "MyString" }
     last_name { "MyString" }
+
+    trait :invalid do
+      first_name { "" }
+      last_name { "" }
+    end
+
+    trait :synced do
+      remote_id { [*1..100000].sample }
+    end
   end
 end
