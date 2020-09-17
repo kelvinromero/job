f0a58fcae75ab56022d04ded403fb107bb023a2b
Kelvin Romero <kelvinromero@gmail.com>
2020-09-16 21:07:09 -0300

    Adds 1 example to sync_user_spec.rb

--- spec/interactors/sync_user_spec.rb
+++ spec/interactors/sync_user_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe SyncUser, type: :interactor do
+  let (:user) { FactoryBot.build(:user) }
+  subject {SyncUser.call(user: user)}
+
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'when context has user' do
+      it 'should be a success' do
+        expect(subject).to be_a_success
+      end
+    end
   end
 end
