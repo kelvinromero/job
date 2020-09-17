e54d49206a50bbe04b3605b14f8ddaf2f743e569
Kelvin Romero <kelvinromero@gmail.com>
2020-09-16 21:07:50 -0300

    Fixes static attributes (rubocop warning)

--- spec/factories/user_params.rb
+++ spec/factories/user_params.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user_params, class: Hash do
-    first_name "MyString"
-    last_name "MyString"
+    first_name { "MyString" }
+    last_name { "MyString" }
 
     trait :invalid do
-      first_name ""
-      last_name ""
+      first_name { "" }
+      last_name { "" }
     end
 
     initialize_with { attributes }
