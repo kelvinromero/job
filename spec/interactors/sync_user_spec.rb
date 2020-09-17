60d4d148509be3909e35ecb976bfa15cd0c3885c
Kelvin Romero <kelvinromero@gmail.com>
2020-09-16 22:39:59 -0300

    When called, interactor should change job count

--- spec/interactors/sync_user_spec.rb
+++ spec/interactors/sync_user_spec.rb
@@ -10,5 +10,12 @@ RSpec.describe SyncUser, type: :interactor do
         expect(subject).to be_a_success
       end
     end
+
+    # TODO: Maybe it can be a shared example, it behaves like SyncUserWorker
+    context 'when invoked' do
+      it 'should change job count' do
+        expect { subject.call }.to change(SyncUserWorker.jobs, :size).by(1)
+      end
+    end
   end
 end
