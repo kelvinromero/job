8843558370b52d4ca034357e81bc6296b2c05220
Kelvin Romero <kelvinromero@gmail.com>
2020-09-16 22:43:48 -0300

    Spec for job count

--- spec/workers/sync_user_worker_spec.rb
+++ spec/workers/sync_user_worker_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe SyncUserWorker, type: :worker do
     described_class.perform_async
     assert_equal :user_sync, described_class.queue
   end
+
+  context 'when invoked' do
+    it 'should change job count' do
+      expect { described_class.perform_async(1, 2) }.to change(described_class.jobs, :size).by(1)
+    end
+  end
 end
